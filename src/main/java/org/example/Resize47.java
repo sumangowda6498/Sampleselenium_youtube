5b9e55fc1f284ef9b7a3aafff6559ac01cb57443
Suman A <sumangowdaarbi@gmail.com>
2024-07-16 23:25:29 +0530

    Commit 47_new commit 16th july

---
+++ src/main/java/org/example/Resize47.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Resize47 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello world!");
+        System.setProperty("webdriver.edge.driver","resources/msedgedriver.exe");
+        WebDriver driver= new EdgeDriver();
+        driver.manage().window().maximize(); //manage: get the option interface
+
+        driver.get("https://jqueryui.com/resizable/");
+        WebElement frame=driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
+        driver.switchTo().frame(frame);
+        WebElement resz=driver.findElement(By.xpath("//*[@id=\"resizable\"]/div[3]"));
+
+        Actions action= new Actions(driver);
+        action.dragAndDropBy(resz,300,100);
+
+
+    }
+}
