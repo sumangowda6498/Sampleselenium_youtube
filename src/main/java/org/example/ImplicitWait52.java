fb499d18879b0e20bde94f265cad4f3f8c8fb182
Raksha B N <rakshanbn20001@gmail.com>
2024-07-23 21:38:56 +0530

    Commit 13 july first check box

---
+++ src/main/java/org/example/ImplicitWait52.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class ImplicitWait52 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello world!");
+        System.setProperty("webdriver.edge.driver","resources/msedgedriver.exe");
+        WebDriver driver= new EdgeDriver();
+        driver.manage().window().maximize(); //manage: get the option interface
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.ebay.com/?msockid=1054b9ace5d066643b20b62be47d6748");
+
+        WebElement webElement=driver.findElement(By.xpath("//*[@id=\"vl-flyout-nav\"]/ul/li[2]/a"));
+
+        Actions action= new Actions(driver);
+        action.moveToElement(webElement).perform();
+        //Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"vl-flyout-nav\"]/ul/li[2]/div[2]/div[1]/nav[1]/ul/li[5]/a")).click();
+
+    }
+}
--- src/main/java/org/example/PrintallLinks38.java
+++ src/main/java/org/example/PrintallLinks38.java
@@ -8,32 +8,29 @@ import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
-import static java.lang.Thread.sleep;
-
-public class PrintallLinks38 {
+public class PrintAlllinks38 {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
-        System.setProperty("webdriver.edge.driver","resources/msedgedriver.exe");
+        System.setProperty("webdriver.edge.driver","C:\\Users\\sumana\\IdeaProjects\\Sampleselenium\\resources\\msedgedriver.exe");
         WebDriver driver= new EdgeDriver();
         driver.manage().window().maximize(); //manage: get the option interface
 
         driver.get("https://www.sugarcrm.com/au/request-demo/");
-        driver.findElement(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")).click();
+       List<WebElement> allTags=driver.findElements(By.tagName("a"));
+        System.out.println("Total Number of tags "+allTags.size());
 
-        sleep(2000);
-
-        List<WebElement> alltags= driver.findElements(By.tagName("a"));
-        System.out.println("Total number of Tags" +alltags.size());
-
-        for(int i=0; i<alltags.size(); i++){
-
-            System.out.println("Links on pages are"+alltags.get(i).getAttribute("href"));
-            System.out.println("The Text on the link"+alltags.get(i).getText());
-            System.out.println();
-
-
-
+        for(int i=0;i<allTags.size();i++){
+            System.out.println("Links on the pages are"+allTags.get(i).getAttribute("href"));
+            System.out.println("Links on the Pages are "+allTags.get(i).getText());
         }
 
+
+        driver.get("https://aws.amazon.com/free/?trk=14a4002d-4936-4343-8211-b5a150ca592b&sc_channel=ps&ef_id=:G:s&s_kwcid=AL!4422!3!!e!!o!!amazon%20web%20services!482208375!1136896004985296&msclkid=e09982d0feb717ef65d3e4a1d659269c&all-free-tier.sort-by=item.additionalFields.SortRank&all-free-tier.sort-order=asc&awsf.Free%20Tier%20Types=*all&awsf.Free%20Tier%20Categories=*all");
+        List<WebElement> allTags1=driver.findElements(By.tagName("img"));
+        System.out.println("Total Number of tags AMAZON "+allTags1.size());
+        for(int i=0;i<allTags1.size();i++){
+            System.out.println("Links on the pages are"+allTags.get(i).getAttribute("src"));
+            //System.out.println("Links on the Pages are "+allTags.get(i).getText());
+        }
     }
 }
